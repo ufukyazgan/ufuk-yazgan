10ad8e77a6ad984c505b173e5cc1de998a812dca
ufukyazgan <ufuk.yazgan@std.yeditepe.edu.tr>
2024-12-25 16:45:11 +0300

    Add files via upload

---
+++ İŞ ALIMI.py
@@ -0,0 +1,6 @@
+yas = int(input("Adayın yaşını giriniz: "))
+notu = float(input("Adayın notunu giriniz: "))
+if 20 <= yas <= 50 and notu > 80:
+    print("Einstellen")  # Aday işe alınır
+else:
+    print("Ablehnen")    # Aday reddedilir
